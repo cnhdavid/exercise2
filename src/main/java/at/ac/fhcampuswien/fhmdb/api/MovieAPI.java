616521057ca0d6195e82ef2f91dc3b9114af6150
Stefan Lackovic <stefan.lackovic@stud.fh-campuswien.ac.at>
2024-04-08 20:58:36 +0200

    push push

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/api/MovieAPI.java
@@ -0,0 +1,63 @@
+package at.ac.fhcampuswien.fhmdb.api;
+import at.ac.fhcampuswien.fhmdb.models.Movie;
+import com.google.gson.reflect.TypeToken;
+import okhttp3.OkHttpClient;
+import com.google.gson.Gson;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieAPI {
+    private final OkHttpClient client = new OkHttpClient();
+    private final Gson gson = new Gson();
+    private final String baseUrl = "https://prog2.fh-campuswien.ac.at/movies";
+
+    public List<Movie> fetchMovies(String searchText, String genre) throws IOException {
+
+        searchText = searchText != null ? searchText : "";
+        genre = genre != null ? genre : "";
+        // URL-Encoding
+        String encodedSearchText = URLEncoder.encode(searchText, "UTF-8");
+        String encodedGenre = URLEncoder.encode(genre, "UTF-8");
+
+
+        String url = baseUrl;
+        List<String> params = new ArrayList<>();
+        if (!searchText.isEmpty()) {
+            params.add("searchText=" + encodedSearchText);
+        }
+        if (!genre.isEmpty()) {
+            params.add("genre=" + encodedGenre);
+        }
+        if (!params.isEmpty()) {
+            url += "?" + String.join("&", params);
+        }
+
+        // Logging der Anfrage-URL fÃ¼r Debugging-Zwecke
+        System.out.println("Request URL: " + url);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("User-Agent", "http.agent")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response + " with body " + response.body().string());
+            }
+            System.out.println("Response" + response);
+            String jsonResponse = response.body().string();
+            System.out.println("Response JSON: " + jsonResponse);
+
+            Type listType = new TypeToken<ArrayList<Movie>>() {}.getType();
+            ArrayList<Movie> movies = gson.fromJson(jsonResponse, listType);
+            System.out.println(movies);
+            return movies != null ? movies : new ArrayList<>();
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,10 @@ module at.ac.fhcampuswien.fhmdb {
     requires javafx.fxml;
 
     requires com.jfoenix;
+    requires com.google.gson;
+    requires okhttp3;
 
     opens at.ac.fhcampuswien.fhmdb to javafx.fxml;
-    exports at.ac.fhcampuswien.fhmdb.models;
+    exports at.ac.fhcampuswien.fhmdb.models to com.google.gson;
     exports at.ac.fhcampuswien.fhmdb;
 }
