a6bead320b9a62c304ffdf45e53609845d1d252e
Stefan Lackovic <stefan.lackovic@stud.fh-campuswien.ac.at>
2024-05-07 16:55:28 +0200

    Dragan

--- src/main/java/at/ac/fhcampuswien/fhmdb/WatchlistController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/WatchlistController.java
@@ -1,15 +1,47 @@
 // WatchlistController.java
 package at.ac.fhcampuswien.fhmdb;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class WatchlistController implements Initializable {
+
+    @FXML
+    public JFXButton homeBtn;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // Initialize watchlist view
         // For example: Load watchlist data and populate TableView
     }
-}
+
+    @FXML
+    public void returnToHomeScreen(ActionEvent actionEvent) {
+        try {
+            // Load the home view FXML file
+            FXMLLoader fxmlLoader1 = new FXMLLoader(FhmdbApplication.class.getResource("home-view.fxml"));
+            Parent homeViewRoot = fxmlLoader1.load();
+
+            // Get the current stage (window) from the event source
+            Stage currentStage = (Stage) homeBtn.getScene().getWindow();
+            currentStage.setWidth(820);
+            currentStage.setHeight(620);
+
+            // Set the root of the current scene to the home view root
+            currentStage.getScene().setRoot(homeViewRoot);
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle any potential errors (e.g., FXML file not found)
+        }
+    }
+    }
+
