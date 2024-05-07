14e8d1c2453346eda19df205a9c1ee2dfa93956f
Stefan Lackovic <stefan.lackovic@stud.fh-campuswien.ac.at>
2024-05-08 00:41:54 +0200

    fix for watchlist.fxml

--- src/main/java/at/ac/fhcampuswien/fhmdb/interfaces/ClickEventHandler.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/interfaces/ClickEventHandler.java
@@ -1,7 +1,7 @@
 package at.ac.fhcampuswien.fhmdb.interfaces;
 
 import java.sql.SQLException;
-
+@FunctionalInterface
 public interface ClickEventHandler<T> {
     void onClick(T item) throws SQLException;
 }
--- src/main/java/at/ac/fhcampuswien/fhmdb/models/ClickEventHandler.java
+++
@@ -1,6 +0,0 @@
-package at.ac.fhcampuswien.fhmdb.models;
-
-@FunctionalInterface
-public interface ClickEventHandler<T> {
-    void onClick(T t);
-}
