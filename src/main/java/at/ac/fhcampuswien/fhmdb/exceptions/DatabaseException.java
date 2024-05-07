f8a82d4db3a1b055a371102b06378b5f53e5838b
cnhdavid <dadi.jovanovic@gmail.com>
2024-05-07 16:29:00 +0200

    gold gold gold

--- src/main/java/at/ac/fhcampuswien/fhmdb/exceptions/DatabaseException.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/exceptions/DatabaseException.java
@@ -1,4 +1,19 @@
 package at.ac.fhcampuswien.fhmdb.exceptions;
 
-public class DatabaseException {
+public class DatabaseException extends Exception {
+    public DatabaseException() {
+        super();
+    }
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+
+    public DatabaseException(Throwable cause) {
+        super(cause);
+    }
+
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/main/java/at/ac/fhcampuswien/fhmdb/exceptions/MovieAPIException.java
+++
@@ -1,4 +0,0 @@
-package at.ac.fhcampuswien.fhmdb.exceptions;
-
-public class MovieAPIException {
-}
